8175bf97676a2fc5236ebb7fa1948f7caaa8f5f0
Ainaseguer <segueraina@gmail.com>
2025-12-12 17:03:16 +0100

    Added docstrings

--- final_project.py
+++ final_project.py
@@ -2,13 +2,29 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
 
-def scale_01(data):
+def scale_01(data:DataFrame)-> DataFrame:
+    """Scales all columns of a DataFrame to the range [0, 1] using MinMaxScaler.
+
+    Args:
+        data (DataFrame): Input DataFrame.
+
+    Returns:
+        DataFrame: Scaled data where each value is in the range [0, 1].
+    """
     scale = MinMaxScaler(feature_range=(0,1))
     data = scale.fit_transform(data)
     return pd.DataFrame(data)
 
 
-def data_processing(data= 'data_given.data'):
+def data_processing(data: str = 'data_given.data') -> DataFrame:
+    """Loads data, transposes, removes first row, scales remaining rows.
+
+    Args:
+        data (str, optional): A file path contaning the data. Defaults to 'data_given.data'.
+
+    Returns:
+        DataFrame: processed dataset
+    """
     data = pd.read_csv(data, header= None)
     data = data.T
     data = data.drop(index=data.index[0])
